6208032ca86012d7fd3c81560c6c5b9dead3b3ea
Ilham Bahtiar <bahtiari073@gmail.com>
2022-01-25 00:42:33 +0800

    Remaps font-size

--- config.def.h
+++ config.def.h
@@ -182,7 +182,7 @@ static MouseShortcut mshortcuts[] = {
 
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
-#define TERMMOD (ControlMask|ShiftMask)
+#define TERMMOD (Mod1Mask|ShiftMask)
 
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
@@ -190,11 +190,14 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
-	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
-	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
-	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
+
+	{ TERMMOD,              XK_plus,       	zoom,           {.f = +1} },
+	{ MODKEY,              	XK_minus,       zoom,           {.f = -1} },
+	{ MODKEY,              	XK_equal,       zoomreset,      {.f =  0} },
+
+	{ MODKEY,              	XK_C,           clipcopy,       {.i =  0} },
+	{ MODKEY,              	XK_V,           clippaste,      {.i =  0} },
+
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
